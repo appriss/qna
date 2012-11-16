5c102397b5a06b236b41d6b1265ac18f502196a6
Alfred J. Fazio <alfred.fazio@gmail.com>
2012-11-16 11:31:00 -0500

    Added explicit RAILS_ENV setting in config/environment.rb.  Checks hostname to see if it starts with "prod".  If so, production, else development.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+ENV["RAILS_ENV"] = %[hostname].start_with?("prod") ? "production" : "development"
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+Rails.env = ENV["RAILS_ENV"]
 
 # Initialize the rails application
 Qna::Application.initialize!
